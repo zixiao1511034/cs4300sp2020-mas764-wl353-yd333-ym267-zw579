e815d98494d1fb8494b992fc02a491abf95c967b
Zixiao Wang <zixiao1511034@outlook.com>
2020-03-28 17:41:44 -0400

    update NetID and Title

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -3,7 +3,7 @@ from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 project_name = "Travel Bucket"
-net_id = "Martin Stoyanov: mas764; Leo Liang: wl353; Yagmur Dukger: yd333; Yash Mundra: ym267; Zixiao Wang: zw579; "
+net_id = ["Martin Stoyanov: mas764"," Leo Liang: wl353"," Yagmur Dukger: yd333","Yash Mundra: ym267"," Zixiao Wang: zw579"]
 
 @irsystem.route('/', methods=['GET'])
 def search():
